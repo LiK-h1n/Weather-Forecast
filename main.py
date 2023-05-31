1b87caa4b4f8b7956422dd8199501ffa21181863
LiK-h1n <likh1n709@gmail.com>
2023-05-31 03:20:56 -0700

    Plot fake data using figures

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from streamlit import title, text_input, slider, selectbox, subheader
+from streamlit import title, text_input, slider, selectbox, subheader, plotly_chart
+from plotly.express import line
 
 title("Weather Forecast for the Next Days")
 place = text_input("Place")
 forecast_days = slider("Forecast Days", 1, 5)
 data_type = selectbox("Data to view", ["Temperature", "Sky"])
 subheader(f"Temperature for the next {forecast_days} {'day' if forecast_days == 1 else 'days'} in {place}")
+dates = ["2023-05-31", "2023-06-01", "2023-06-02"]
+temperatures = [3, 10, 4]
+figure = line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+plotly_chart(figure)
