925f4b7d857229a677e17fd8d56ae40ea0fd7d5a
LiK-h1n <likh1n709@gmail.com>
2023-05-31 05:02:23 -0700

    Generates temperature graph and sky condition images

--- backend.py
+++ backend.py
@@ -3,11 +3,14 @@ from requests import get
 API_KEY = "eb803fd0e96ff09195f9dc981a0ecba8"
 
 
-def get_data(place, forecast_days, kind):
+def get_data(place, forecast_days):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = get(url)
     data = response.json()
-    return data
+    filtered_data = data["list"]
+    no_values = 8 * forecast_days
+    filtered_data = filtered_data[:no_values]
+    return filtered_data
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-from streamlit import title, text_input, slider, selectbox, subheader, plotly_chart
+from streamlit import title, text_input, slider, selectbox, subheader, plotly_chart, image
 from plotly.express import line
+from backend import get_data
 
 title("Weather Forecast for the Next Days")
 place = text_input("Place")
 forecast_days = slider("Forecast Days", 1, 5)
 kind = selectbox("Data to view", ["Temperature", "Sky"])
-subheader(f"Temperature for the next {forecast_days} {'day' if forecast_days == 1 else 'days'} in {place}")
-dates = ["2023-05-31", "2023-06-01", "2023-06-02"]
-temperatures = [3, 10, 4]
-figure = line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
-plotly_chart(figure)
+subheader(f"{kind} for the next {forecast_days if forecast_days > 1 else ''} {'day' if forecast_days == 1 else 'days'} "
+          f"in {place}")
+
+if place:
+    filtered_data = get_data(place, forecast_days)
+
+    if kind == "Temperature":
+        temperatures = [value["main"]["temp"] for value in filtered_data]
+        dates = [value["dt_txt"] for value in filtered_data]
+        figure = line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+        plotly_chart(figure)
+    elif kind == "Sky":
+        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png", "Rain": "images/rain.png",
+                  "Snow": "images/snow.png"}
+        sky_conditions = [value["weather"][0]["main"] for value in filtered_data]
+        image_paths = [images[sky_condition] for sky_condition in sky_conditions]
+        image(image_paths, width=115)
